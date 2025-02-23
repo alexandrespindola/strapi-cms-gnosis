bd6c425c0f0992ce107e4f4fff2d08d997663be6
Alexandre Spindola <alexandre.spindola11@gmail.com>
2025-02-23 15:13:39 +0100

    adiciona estrutura inicial da API de autores e configurações de segurança e CORS

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,28 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      header: '*',
+      origin: ['http://localhost:3000', 'https://railwayapp-strapi-production-3ea7.up.railway.app']
+    }
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
